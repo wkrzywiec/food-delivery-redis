81e7a2f13941649d2859f1b4e5a03de6737aeb84
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2022-07-24 15:30:45 +0200

    Integration tests (#3)
    * feature branch actions
    
    * sonar badges
    
    * correct names of reusable wfs
    
    * add postgres db
    
    * integration test base class
    
    * add first integration tests
    
    * logs formatting
    
    * dev and prod spring profiles

---
+++ src/integration-test/groovy/io/wkrzywiec/fooddelivery/FoodDeliveryApplicationSpec.groovy
@@ -0,0 +1,15 @@
+package io.wkrzywiec.fooddelivery
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+
+class FoodDeliveryApplicationSpec extends IntegrationTest {
+
+    @Autowired
+    ApplicationContext context
+
+    def "should load full Spring context"() {
+        expect: "Spring context is loaded correctly"
+        context
+    }
+}

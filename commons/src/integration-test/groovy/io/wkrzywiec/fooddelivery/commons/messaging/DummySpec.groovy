20bddb9b5c22752d64254133053db1716ed692e2
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2022-08-11 12:41:51 +0200

    add dummy test for commons

---
+++ commons/src/integration-test/groovy/io/wkrzywiec/fooddelivery/commons/messaging/DummySpec.groovy
@@ -0,0 +1,15 @@
+package io.wkrzywiec.fooddelivery.commons.messaging
+
+
+import spock.lang.Specification
+
+class DummySpec extends Specification {
+
+    def "This is a dummy test since this module dont have other tests yet"() {
+        when:
+        System.out.println("do dummy thing")
+
+        then:
+        1 == 1
+    }
+}

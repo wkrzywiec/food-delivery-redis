c7231653b3a31c5046a84d104ad62a4fb5bfa257
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2022-08-14 10:46:16 +0200

    use local infra in integration test

--- commons/src/main/groovy/io/wkrzywiec/fooddelivery/commons/IntegrationTest.groovy
+++ commons/src/main/groovy/io/wkrzywiec/fooddelivery/commons/IntegrationTest.groovy
@@ -10,8 +10,10 @@ import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.web.context.WebApplicationContext
+import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.spock.Testcontainers
+import org.testcontainers.utility.DockerImageName
 import spock.lang.Specification
 
 @Testcontainers
@@ -24,12 +26,28 @@ abstract class IntegrationTest extends Specification {
     private static final String DB_USERNAME = "food_delivery"
     private static final String DB_PASSWORD = "food_delivery"
 
+    private static final GenericContainer REDIS
+
     static {
+
+        if (useLocalInfrastructure()) {
+
+        }
         POSTGRES_DB = new PostgreSQLContainer("postgres:14-alpine")
                 .withDatabaseName(DB_NAME)
                 .withUsername(DB_USERNAME)
                 .withPassword(DB_PASSWORD)
         POSTGRES_DB.start()
+
+        REDIS = new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+                .withExposedPorts(6379)
+        REDIS.start()
+    }
+
+
+    static boolean useLocalInfrastructure() {
+        // change it to `true` in order to use it with infra from docker-compose.yaml
+        false
     }
 
     @Autowired
@@ -51,6 +69,8 @@ abstract class IntegrationTest extends Specification {
                     "spring.datasource.url=" + POSTGRES_DB.getJdbcUrl(),
                     "spring.datasource.username=" + DB_USERNAME,
                     "spring.datasource.password=" + DB_PASSWORD,
+                    "spring.redis.host=" + REDIS.getHost(),
+                    "spring.redis.port=" + REDIS.getMappedPort(6379)
             )
 
             values.applyTo(applicationContext)
